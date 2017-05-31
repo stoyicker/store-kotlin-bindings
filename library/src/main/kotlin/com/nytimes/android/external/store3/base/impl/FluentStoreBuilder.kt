1ac8f5528b2ee18171d89316abad03b54b61f60a
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-05-31 20:22:21 +0200

    Allow Parser to be specified only when Raw != Parsed

--- library/src/main/kotlin/com/nytimes/android/external/store3/base/impl/FluentStoreBuilder.kt
+++ library/src/main/kotlin/com/nytimes/android/external/store3/base/impl/FluentStoreBuilder.kt
@@ -1,41 +1,13 @@
+@file:JvmName("DemoUtils")
+
 package com.nytimes.android.external.store3.base.impl
 
 import com.nytimes.android.external.store3.base.Fetcher
-import com.nytimes.android.external.store3.base.Parser
-import com.nytimes.android.external.store3.base.Persister
-import com.nytimes.android.external.store3.util.KeyParser
 
 /**
  * Wraps methods for fluent Store instantiation.
  */
-class FluentStoreBuilder<Raw, Parsed, Key> private constructor(
-        private val fetcher: Fetcher<Raw, Key>) {
-    var persister: Persister<Raw, Key>? = null
-    var parser: KeyParser<Key, Raw, Parsed>? = null
-        set(value) {
-            field = value
-            parsers = null
-        }
-    var parsers: List<Parser<Raw, Parsed>>? = null
-        set(value) {
-            field = value
-            parser = null
-        }
-    var memoryPolicy: MemoryPolicy? = null
-
-    /**
-     * Applies any given configuration for the Store that will be created.
-     * @param config Optional configuration for the store to instantiate.
-     * @return The created Store with the given configuration, if any.
-     */
-    internal fun apply(config: (FluentStoreBuilder<Raw, Parsed, Key>.() -> Unit)?)
-            : Store<Parsed, Key> {
-        if (config != null) {
-            this.config()
-        }
-        return FluentRealStoreBuilder(fetcher, persister, parser, parsers, memoryPolicy).open()
-    }
-
+class FluentStoreBuilder {
     companion object {
         /**
          * Provides a fluent builder to instantiate a Store that uses BarCode objects as keys.
@@ -44,8 +16,8 @@ class FluentStoreBuilder<Raw, Parsed, Key> private constructor(
          */
         fun <Parsed> barcode(
                 fetcher: Fetcher<Parsed, BarCode>,
-                config: (FluentStoreBuilder<Parsed, Parsed, BarCode>.() -> Unit)? = null)
-                = parsedWithKey(fetcher, config)
+                config: (StoreParameters<Parsed, BarCode>.() -> Unit)? = null) =
+                key(fetcher, config)
 
         /**
          * Provides a fluent builder to instantiate a Store with a custom type for keys.
@@ -54,8 +26,16 @@ class FluentStoreBuilder<Raw, Parsed, Key> private constructor(
          */
         fun <Parsed, Key> key(
                 fetcher: Fetcher<Parsed, Key>,
-                config: (FluentStoreBuilder<Parsed, Parsed, Key>.() -> Unit)? = null)
-                = parsedWithKey(fetcher, config)
+                config: (StoreParameters<Parsed,  Key>.() -> Unit)? = null) =
+                StoreParameters(fetcher).apply {
+                    if (config != null) {
+                        this.config()
+                    }
+                }.let {
+                    FluentRealStoreBuilder<Parsed, Parsed, Key>(
+                            fetcher, it.persister, null, null, it.memoryPolicy)
+                            .open()
+                }
 
         /**
          * Provides a fluent builder to instantiate a Store with a custom type for keys and
@@ -65,7 +45,15 @@ class FluentStoreBuilder<Raw, Parsed, Key> private constructor(
          */
         fun <Raw, Parsed, Key> parsedWithKey(
                 fetcher: Fetcher<Raw, Key>,
-                config: (FluentStoreBuilder<Raw, Parsed, Key>.() -> Unit)? = null)
-                = FluentStoreBuilder<Raw, Parsed, Key>(fetcher).apply(config)
+                config: (ParsableStoreParameters<Raw, Parsed, Key>.() -> Unit)? = null) =
+                ParsableStoreParameters<Raw, Parsed, Key>(fetcher).apply {
+                    if (config != null) {
+                        this.config()
+                    }
+                }.let {
+                    FluentRealStoreBuilder(
+                            fetcher, it.persister, it.parser, it.parsers, it.memoryPolicy)
+                            .open()
+                }
     }
 }
