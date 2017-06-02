c08e9665744a0575c3dd75eb65585a1a94ea128b
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2017-06-02 18:31:22 +0200

    Make MemoryPolicy parameters vetoable

--- library/src/main/kotlin/com/nytimes/android/external/store3/base/impl/MemoryPolicyParameters.kt
+++ library/src/main/kotlin/com/nytimes/android/external/store3/base/impl/MemoryPolicyParameters.kt
@@ -1,13 +1,20 @@
 package com.nytimes.android.external.store3.base.impl
 
 import java.util.concurrent.TimeUnit
+import kotlin.properties.Delegates
 
 /**
  * A parameter box for MemoryPolicy instantiation.
  */
 class MemoryPolicyParameters {
-    var expireAfterWrite = MemoryPolicy.DEFAULT_POLICY
-    var expireAfterAccess = MemoryPolicy.DEFAULT_POLICY
+    var expireAfterWrite by Delegates.vetoable(MemoryPolicy.DEFAULT_POLICY) {
+        _, _, newValue -> newValue >= 0
+    }
+    var expireAfterAccess by Delegates.vetoable(MemoryPolicy.DEFAULT_POLICY) {
+        _, _, newValue -> newValue >= 0
+    }
     var expireAfterTimeUnit = TimeUnit.SECONDS
-    var maxSize = 1L
+    var maxSize by Delegates.vetoable(1L) {
+        _, _, newValue -> newValue >= 1
+    }
 }
